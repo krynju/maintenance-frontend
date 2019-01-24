c52d10775d5c87bcac77e7401ac9f0e9ae1efbdf
Krystian Gulinski <gulinski.krystian@gmail.com>
2019-01-24 23:04:59 +0100

    add login by code

--- src/app/layout/components/topnav/topnav.component.ts
+++ src/app/layout/components/topnav/topnav.component.ts
@@ -34,6 +34,7 @@ export class TopnavComponent implements OnInit {
 
   onLoggedout() {
     localStorage.removeItem('isLoggedin');
+    localStorage.removeItem('userData');
     this.router.navigate(['/login']);
   }
 }
