fb882716c6951c274bccc27550d420abf7871707
Krystian Gulinski <gulinski.krystian@gmail.com>
2019-01-24 19:27:44 +0100

    add stuff

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import {CommonModule} from '@angular/common';
 import {TicketListComponent} from './components/ticket-list/ticket-list.component';
 import {FailureListComponent} from './components/failure-list/failure-list.component';
 import {DemoMaterialModule} from '../material_module';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [TicketListComponent, FailureListComponent],
   imports: [
     CommonModule,
     DemoMaterialModule,
+    FlexLayoutModule
   ],
   exports: [
     TicketListComponent,
